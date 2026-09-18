121c9a4a19901b6c9d05a35911723680f2a970a1
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-18 10:33:07 -0300

    feat: traduz pagina de AboutUs

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,29 +1,37 @@
-import React from 'react';
-import './AboutUs.css';
+import React from "react";
+import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us-container">
-      {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
-      <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+      {/* <h1 className="about-us-heading">Sobre nós</h1> */}
+      <p className="about-us-description">
+        Bem-vindo ao Paradise Nursery, onde o verde encontra a serenidade!
+      </p>
+      <p className="sobre-nos-content">
+        Na Paradise Nursery, somos apaixonados por trazer a natureza para mais
+        perto de você. Nossa missão é fornecer uma ampla gama de plantas de alta
+        qualidade que não só realçam a beleza do seu entorno, mas também
+        contribuem para uma vida mais saudável e estilo de vida mais
+        sustentável. De plantas purificadoras de ar a plantas aromáticas e
+        perfumadas, temos algo para cada entusiasta de plantas.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-      <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+      <p className="sobre-nos-content">
+        Nossa equipe de especialistas se dedica a garantir que cada planta
+        atenda aos nossos rígidos padrões de qualidade e cuidado. Quer você seja
+        um jardineiro experiente ou esteja apenas começando sua jornada
+        ecológica, estamos aqui para apoiá-lo em cada etapa do processo. o
+        caminho. Sinta-se à vontade para explorar nossa coleção, fazer perguntas
+        e deixar-nos ajudá-lo a encontrar a planta perfeita para o seu casa ou
+        escritório.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
-      <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+      <p className="sobre-nos-content">
+        Junte-se a nós em nossa missão de criar um mundo mais verde e saudável.
+        Visite Paradise Nursery hoje e experimente o beleza da natureza bem à
+        sua porta.
       </p>
     </div>
   );
