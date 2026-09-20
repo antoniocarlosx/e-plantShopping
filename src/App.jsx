bed4ec93f2a61fa513d46e4a322deeb51d2d6119
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-20 14:17:24 -0300

    refactor: cria um array especifico para plants

--- src/App.jsx
+++ src/App.jsx
@@ -15,33 +15,43 @@ function App() {
   };
 
   return (
-    <div className="app-container">
-      <div className={`landing-page ${showProductList ? "fade-out" : ""}`}>
-        <div className="content">
-          <div className="landing_content">
-            <h1 className="content-title">Bem vindo ao <br /><strong className="title-emfase">Paradise Nursery</strong></h1>
-            <div className="divider"></div>
-            <p className="content-subtitle"><em>Onde o <strong className="subtitle-emfase">verde</strong> encontra a <strong className="subtitle-emfase">serenidade</strong></em></p>
+    <>
+    {/*Enquanto o botão não foi clicado a landing page fica vizivel*/}
+      {!showProductList && (
+        <header className="landing-page ">
+          <div className="content">
+            <div className="landing_content">
+              <h1 className="content-title">
+                Bem vindo ao <br />
+                <strong className="title-emfase">Paradise Nursery</strong>
+              </h1>
+              <div className="divider"></div>
+              <p className="content-subtitle">
+                <em>
+                  Onde o <strong className="subtitle-emfase">verde</strong>{" "}
+                  encontra a{" "}
+                  <strong className="subtitle-emfase">serenidade</strong>
+                </em>
+              </p>
+            </div>
+
+            <div className="aboutus_container">
+              <AboutUs />
+            </div>
+            <div className="button-container">
+              <button
+                className="get-started-button"
+                onClick={handleGetStartedClick}
+              >
+                Começar Agora
+              </button>
+            </div>
           </div>
-          <div className="aboutus_container">
-            <AboutUs />
-          </div>
-          <div className="button-container">
-            <button
-              className="get-started-button"
-              onClick={handleGetStartedClick}
-            >
-              Começar Agora
-            </button>
-          </div>
-        </div>
-      </div>
-      <div
-        className={`product-list-container ${showProductList ? "visible" : ""}`}
-      >
-        <ProductList onHomeClick={handleHomeClick} />
-      </div>
-    </div>
+        </header>
+      )}
+       {/*Quando clica a página some*/}
+      {showProductList && <ProductList onHomeClick={handleHomeClick} />}
+    </>
   );
 }
 
---
+++ src/NavBar.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./NavBar.css";
+
+const NavBar = ({ links, appName, iconLink, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <header className="navbar-header">
+        <div className="nav-title">
+          <div className="title-container">
+            <img className="app-icon" src={iconLink} alt={appName} />
+            <span id="home" className="app-name">
+              {appName}
+            </span>
+            
+          </div>
+          <button
+            type="button"
+            className={`menu-toggler ${isOpen ? "active" : ""}`}
+            onClick={() => setIsOpen((prev) => !prev)}
+            aria-label="Abrir Menu"
+          >
+            <span className="hamburger-line" />
+            <span className="hamburger-line" />
+            <span className="hamburger-line" />
+          </button>
+        </div>
+        <nav className={`nav-menu ${isOpen ? "active" : ""}`}>
+          <ul className="nav-list">
+            {links?.map((link) => (
+              <li key={link.href} className="nav-list-item">
+                <a
+                  href={link.href}
+                  className="nav-link"
+                  onClick={() => setIsOpen(false)}
+                >
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="nav-actions">{children}</div>
+      </header>
+    </>
+  );
+};
+
+export default NavBar;
