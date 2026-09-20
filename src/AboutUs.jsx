3bc1333b6e1fc0c816f74aef2313b72d0d5dbf6e
Antonio Carlos <xdatabycarlos@gmail.com>
2026-09-20 11:23:37 -0300

    feat: melhora a estilização da landing page

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -6,30 +6,16 @@ function AboutUs() {
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">Sobre nós</h1> */}
       <p className="about-us-description">
-        Bem-vindo ao Paradise Nursery, onde o verde encontra a serenidade!
-      </p>
-      <p className="sobre-nos-content">
-        Na Paradise Nursery, somos apaixonados por trazer a natureza para mais
-        perto de você. Nossa missão é fornecer uma ampla gama de plantas de alta
-        qualidade que não só realçam a beleza do seu entorno, mas também
-        contribuem para uma vida mais saudável e estilo de vida mais
-        sustentável. De plantas purificadoras de ar a plantas aromáticas e
-        perfumadas, temos algo para cada entusiasta de plantas.
-      </p>
-      {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-      <p className="sobre-nos-content">
-        Nossa equipe de especialistas se dedica a garantir que cada planta
-        atenda aos nossos rígidos padrões de qualidade e cuidado. Quer você seja
-        um jardineiro experiente ou esteja apenas começando sua jornada
-        ecológica, estamos aqui para apoiá-lo em cada etapa do processo. o
-        caminho. Sinta-se à vontade para explorar nossa coleção, fazer perguntas
-        e deixar-nos ajudá-lo a encontrar a planta perfeita para o seu casa ou
-        escritório.
-      </p>
-      {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
-
-      <p className="sobre-nos-content">
-        Junte-se a nós em nossa missão de criar um mundo mais verde e saudável.
+        Na Paradise Nursery, unimos paixão pela natureza e dedicação botânica
+        para trazer mais serenidade e equilíbrio ao seu dia a dia. <br /> <br />{" "}
+        Selecionamos e cultivamos plantas de alta qualidade — de espécies
+        purificadoras de ar a aromáticas exclusivas —, prestando suporte
+        especializado tanto a iniciantes quanto a apaixonados por jardinagem.{" "}
+        <br /> <br />
+        Sinta-se à vontade para explorar nossa coleção, fazer perguntas e
+        deixar-nos ajudá-lo a encontrar a planta perfeita para o seu casa ou
+        escritório. Junte-se a nós em nossa missão de criar um mundo mais verde
+        e saudável. <br /> <br />
         Visite Paradise Nursery hoje e experimente o beleza da natureza bem à
         sua porta.
       </p>
